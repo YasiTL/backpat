6f839cad599d85f32fd974151edf15edc70e224d
Joshua Prila <55035345+AlirpJ@users.noreply.github.com>
2020-08-31 11:36:59 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> and save to reload. This is the backpat site home page!
         </p>
         <Button color="primary">Test</Button> 
         <a
