af846fdc842b6e4e5ad2ffe429783946a75e368b
Joshua Prila <55035345+AlirpJ@users.noreply.github.com>
2020-09-03 00:09:18 -0500

    Add files via upload

---
+++ src/components/TutorSignUp.js
@@ -0,0 +1,15 @@
+import react from 'react';
+
+
+function TutorSignUp(){
+    return (
+        <div>
+
+            Tutor Sign Up Component!
+
+        </div>
+    );
+}
+
+
+export default TutorSignUp
