46eb38f901a027ab8b9640d566683f960d0b6059
Joshua Prila <55035345+AlirpJ@users.noreply.github.com>
2020-09-09 15:55:01 -0500

    Update TutorSignUp.js

--- src/components/TutorSignUp.js
+++ src/components/TutorSignUp.js
@@ -1,15 +1,19 @@
 import React from 'react';
+import { render } from '@testing-library/react';
+import TutorForm from './TutorForm';
 
 
 function TutorSignUp(){
+
+    
     return (
         <div>
 
             Tutor Sign Up Component!
-
+        <TutorForm />
         </div>
     );
-}
 
+}
 
 export default TutorSignUp;
