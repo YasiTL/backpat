3495bb6c308c1fae80814856e299a15370f9ea2a
Joshua Prila <55035345+AlirpJ@users.noreply.github.com>
2020-09-14 11:34:53 -0500

    Update ParentSignUp.js

--- src/components/ParentSignUp.js
+++ src/components/ParentSignUp.js
@@ -1,10 +1,11 @@
 import React from 'react';
-
+import ParentForm from './ParentForm';
 
 function ParentSignUp() {
     return (
         <div>
             Parent Sign up component!
+            <ParentForm />
         </div>
     );
 }
