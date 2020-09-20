c0e65f6c24ece60a7784bf68c2b7afdec2796238
SeeLtephen <32529372+SeeLtephen@users.noreply.github.com>
2020-09-19 22:28:53 -0400

    Tutor Homepage Component Creation

---
+++ src/components/TutorHomepage.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+
+
+function TutorHomepage(){
+
+    
+    return (
+        <div>
+            Tutor Homepage Component!
+        </div>
+    );
+
+}
+
+export default TutorHomepage;
