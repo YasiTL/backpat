811b69bf0beb9af01a561267c568c0e7db4b9324
Joshua Prila <55035345+AlirpJ@users.noreply.github.com>
2020-10-04 21:42:55 -0500

    Update ParentSignUp.js

--- src/components/ParentSignUp.js
+++ src/components/ParentSignUp.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import ParentForm from './ParentForm';
+import {
+    BrowserRouter as Router,
+    Link,
+  } from "react-router-dom"
 
 function ParentSignUp() {
     return (
         <div>
+            <li>
+                <Link to="/">Home</Link>
+              </li>
             Parent Sign up component!
             <ParentForm />
+            
         </div>
     );
 }
