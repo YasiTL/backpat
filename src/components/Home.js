b385c7002d919330843789dc1f465603c8a83f52
Joshua Prila <55035345+AlirpJ@users.noreply.github.com>
2020-10-04 21:39:10 -0500

    Update Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -17,8 +17,12 @@ function Home() {
                 <Link to="/parentsignup">Parent Sign Up</Link>
               </li>
               <li>
+                <Link to="/tutorsignup">Tutor Sign Up</Link>
+              </li>
+              <li>
                 <Link to="/login">Login here</Link>
               </li>
+
             </ul>
           </nav>
         </div>
