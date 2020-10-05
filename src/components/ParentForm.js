d130e511308b7736ef67446776b75a123adeed3d
Joshua Prila <55035345+AlirpJ@users.noreply.github.com>
2020-10-04 21:42:32 -0500

    Update ParentForm.js

--- src/components/ParentForm.js
+++ src/components/ParentForm.js
@@ -1,6 +1,6 @@
 import   React, { Component } from 'react'
 
-class ParentForm extends React.Component {
+class TutorForm extends React.Component {
     constructor(props){
         super(props);
         this.state = {
@@ -8,8 +8,9 @@ class ParentForm extends React.Component {
             lastName: "doe",
             email: "blank@mailcatch.com",
             phone: 1234567890,
+            university: "Illinois Institute of Technology",
             timezone: "CST",
-            essential: true
+            bio: "beep boop"
 
         };
         //this.handleInputChange = this.handleInputChange.bind(this);
@@ -28,7 +29,9 @@ render() {
                 name="firstName"
                 type="text"
                 value={this.state.firstName}
-                onChange={e => this.setState({ firstName: e.target.value})} />
+                //onChange={this.handleInputChange} 
+                onChange={e => this.setState({ firstName: e.target.value})}
+                />
 
             </label>
             <br />
@@ -64,6 +67,16 @@ render() {
             </label>
             <br />
             <label>
+                University:
+                <input
+                name="university"
+                type="text"
+                value={this.state.university}
+                onChange={e => this.setState({ university: e.target.value})} />
+
+            </label>
+            <br />
+            <label>
                 Timezone:
                 <input
                 name="timezone"
@@ -73,58 +86,19 @@ render() {
 
             </label>
             <br />
+            <label>
+                Short Bio:
+                <input
+                name="bio"
+                type="text"
+                value={this.state.bio}
+                onChange={e => this.setState({ bio: e.target.value})} />
+
+            </label>
             </div>
-
-
-            <div className="form-check">
-    <label>
-        Are you an Essential Worker?
-        <br />
-      <input
-        type="radio"
-        name="essential"
-        value="yes"
-        
-        
-        checked={this.state.selectedOption === "yes"}
-        onChange = {this.handleCheck}
-        
-        className="form-check-input"
-      />
-      Yes
-    </label>
-  </div>
-
-  <div className="form-check">
-    <label>
-      <input
-        type="radio"
-        name="essential"
-        value="no"
-        
-        checked={this.state.selectedOption === "no"}
-        onChange = {this.handleCheck}
-        
-        className="form-check-input"
-      />
-      No
-    </label>
-  </div>
-
-
-
 
         </form>
     )
 }
     }
-
-export default ParentForm
-
-// I don't know how to make the buttons show which one you last clicked
-// http://react.tips/radio-buttons-in-react-16/
-//handleOptionChange = changeEvent => {
-//    this.setState({
-//        selectedOption: changeEvent.target.value
-//      });
-//    };
+export default TutorForm
