5feee85c1dedd6e328c658641e98e52bc24d72ce
Yasaman Mostafavi <yasaman.most@gmail.com>
2020-10-08 22:16:03 -0500

    hook up firebase with react app

--- src/index.js
+++ src/index.js
@@ -4,9 +4,13 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+import Firebase, { FirebaseContext } from './components/Firebase';
+
 ReactDOM.render(
   <React.StrictMode>
+    <FirebaseContext.Provider value={new Firebase()}>
     <App />
+    </FirebaseContext.Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
