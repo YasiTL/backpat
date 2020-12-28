4a362f08d798256b05ced13f23578407a101492b
YasiTL <yasaman.most@gmail.com>
2020-12-27 21:38:57 -0600

    tutor form styling

--- src/components/TutorForm.js
+++ src/components/TutorForm.js
@@ -1,4 +1,6 @@
-import   React, { Component } from 'react'
+import React from 'react'
+import { TextField, Button } from '@material-ui/core';
+import InputMask from 'react-input-mask';
 
 class TutorForm extends React.Component {
     constructor(props){
@@ -7,11 +9,10 @@ class TutorForm extends React.Component {
             firstName: "john",
             lastName: "doe",
             email: "blank@mailcatch.com",
-            phone: 1234567890,
+            phone: "1234567890",
             university: "Illinois Institute of Technology",
             timezone: "CST",
             bio: "beep boop"
-
         };
         //this.handleInputChange = this.handleInputChange.bind(this);
     }
@@ -25,91 +26,80 @@ class TutorForm extends React.Component {
 
 render() {
     console.log("TutorForm.js")
-    return ( 
-        
-
-        <form onSubmit ={this.handleSubmit}
-        id = "TutorForm">
+    return (
+        <form onSubmit={this.handleSubmit} id="ParentForm" autoComplete="off">
             <div>
-            <label>
-                First Name:
-                <input
-                name="firstName"
-                type="text"
-                value={this.state.firstName}
-                onChange={e => this.setState({ firstName: e.target.value})}
-                //onChange={this.handleInputChange} 
+                <TextField
+                    required
+                    name="firstName"
+                    type="text"
+                    value={this.state.firstName}
+                    label="First Name"
+                    onChange={e => this.setState({ firstName: e.target.value })}
                 />
-
-            </label>
-            <br />
-            <label>
-                Last Name:
-                <input
-                name="lastName"
-                type="text"
-                value={this.state.lastName}
-                onChange={e => this.setState({ lastName: e.target.value})}
-                //onChange={this.handleInputChange} 
+                <br />
+                <TextField
+                    required
+                    name="lastName"
+                    type="text"
+                    value={this.state.lastName}
+                    label="Last Name"
+                    onChange={e => this.setState({ lastName: e.target.value })}
                 />
-
-
-            </label>
-            <br />
-            <label>
-                Email:
-                <input
-                name="email"
-                type="email"
-                value={this.state.email}
-                onChange={e => this.setState({ email: e.target.value})}
-                //onChange={this.handleInputChange} 
+                <br />
+                <TextField
+                        required
+                        name="email"
+                        type="text"
+                        value={this.state.email}
+                        label="Email"
+                        onChange={e => this.setState({ email: e.target.value })}
+                    />
+                <br />
+                <InputMask
+                    mask="(999)-999-9999"
+                    value={this.state.phone}
+                    onChange={e => this.setState({ phone: e.target.value })}
+                    >
+                    <TextField
+                        required
+                        name="phone"
+                        label="Phone"
+                        ref={this.inputRef}
+                        type="text"/>
+                </InputMask>
+                <br />
+                <TextField
+                    required
+                    name="university"
+                    type="text"
+                    value={this.state.university}
+                    label="Unviersity"
+                    onChange={e => this.setState({ university: e.target.value })}
                 />
-
-            </label>
-            <br />
-            <label>
-                Phone:
-                <input
-                name="phone"
-                type="number"
-                value={this.state.phone}
-                onChange={e => this.setState({ phone: e.target.value})} />
-
-            </label>
-            <br />
-            <label>
-                University:
-                <input
-                name="university"
-                type="text"
-                value={this.state.university}
-                onChange={e => this.setState({ university: e.target.value})} />
-
-            </label>
-            <br />
-            <label>
-                Timezone:
-                <input
-                name="timezone"
-                type="text"
-                value={this.state.timezone}
-                onChange={e => this.setState({ timezone: e.target.value})} />
-
-            </label>
-            <br />
-            <label>
-                Short Bio:
-                <input
-                name="bio"
-                type="text"
-                value={this.state.bio}
-                onChange={e => this.setState({ bio: e.target.value})} />
-
-            </label>
+                <br />
+                <TextField
+                    required
+                    name="timezone"
+                    type="text"
+                    value={this.state.timezone}
+                    label="Timezone"
+                    onChange={e => this.setState({ timezone: e.target.value })}
+                />
+                <br />
+                <TextField
+                    required
+                    multiline
+                    rows={4}
+                    name="bio"
+                    type="text"
+                    value={this.state.bio}
+                    label="Short Bio"
+                    onChange={e => this.setState({ bio: e.target.value })}
+                />
             </div>
-            <p class = "full">
-                <button type="submit">Submit</button>
+            <p className="full">
+                <Button variant="contained" color="primary" type="submit">Submit</Button>
             </p>
         </form>
     )
