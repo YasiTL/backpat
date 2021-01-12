bbccc94b89893f6b2568ea3c36198f724886d3f8
YasiTL <yasaman.most@gmail.com>
2021-01-11 21:42:10 -0600

    fix git ignore and firebase.js to use correct config from .env file in root folder

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -1,18 +1,17 @@
 import firebase from 'firebase';
-import config from './firebaseconfig.env'
 
-const firebaseConfig = {
-  apiKey: config.apiKey,
-  authDomain: config.authDomain,
-  databaseURL: config.databaseURL,
-  projectId: config.projectId,
-  storageBucket: config.storageBucket,
-  messagingSenderId: config.messagingSenderId,
-  appId: config.appId,
-  measurementId: config.measurementId
+
+export const firebaseConfig = {
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID
 };
 
 firebase.initializeApp(firebaseConfig);
-
 export const auth = firebase.auth;
 export const db = firebase.firestore();
