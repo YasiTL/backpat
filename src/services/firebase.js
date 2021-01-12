a4ea8022c4c01262edc7ad91b1e206d8b2154374
YasiTL <yasaman.most@gmail.com>
2021-01-11 21:16:55 -0600

    use firebaseconfig instead of pasting plain strings

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -1,7 +1,15 @@
 import firebase from 'firebase';
+import config from './firebaseconfig.env'
 
 const firebaseConfig = {
-    // PUT CONFIG FROM FIREBASE HERE
+  apiKey: config.apiKey,
+  authDomain: config.authDomain,
+  databaseURL: config.databaseURL,
+  projectId: config.projectId,
+  storageBucket: config.storageBucket,
+  messagingSenderId: config.messagingSenderId,
+  appId: config.appId,
+  measurementId: config.measurementId
 };
 
 firebase.initializeApp(firebaseConfig);
