671b93d28bf020ff6148cd8070461ba116f819fd
YasiTL <yasaman.most@gmail.com>
2021-03-06 11:29:26 -0600

    resolve warnings

--- src/components/Profile/index.jsx
+++ src/components/Profile/index.jsx
@@ -3,7 +3,6 @@ import Sidenav from '../Dashboard/Sidenav';
 import BasicInfoSection from './BasicInfoSection';
 import { Typography } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
-import TutoringInfoSection from './TutoringInfoSection';
 import ParentInfoSection from './ParentInfoSection';
 
 const useStyles = makeStyles((theme) => ({
