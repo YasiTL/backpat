d82cf1d24df8bafd7f2cda35e83a9a0fbae0e950
YasiTL <yasaman.most@gmail.com>
2021-04-19 17:41:41 -0500

    start up admin login

--- src/components/AdminLogin.js
+++ src/components/AdminLogin.js
@@ -1,7 +1,25 @@
 import React from 'react'
+import firebase from 'firebase'
+require('firebase/auth')
+var firebaseui = require('firebaseui');
+
+
+var ui = new firebaseui.auth.AuthUI(firebase.auth());
+
+ui.start('#firebaseui-auth-container', {
+  signInOptions: [
+    firebase.auth.EmailAuthProvider.PROVIDER_ID,
+    firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+  ],
+  signInSuccessUrl: `${process.env.PUBLIC_URL}/admindashboard`,
+  tosUrl: '', //terms of service URL
+  privacyPolicyUrl: '' //privacy policy URL
+});
+
 function AdminLogin() {
   return (
       <div>
+        <div id="firebaseui-auth-container"></div>
           Admin Login
       </div>
   );
