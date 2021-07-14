c491b1514d16bf3d30bb6af6fe44737b157bfd4f
YasiTL <yasaman.most@gmail.com>
2021-07-13 20:11:15 -0500

    Remove unused imports and row function

--- src/components/AdminDashboard/AdminDashboard.js
+++ src/components/AdminDashboard/AdminDashboard.js
@@ -11,62 +11,6 @@ import Box from '@material-ui/core/Box';
 
 import UserTable from './UserTable';
 
-import TableRow from '@material-ui/core/TableRow';
-import TableCell from '@material-ui/core/TableCell';
-import TableContainer from '@material-ui/core/TableContainer';
-import Table from '@material-ui/core/Table';
-import TableHead from '@material-ui/core/TableHead';
-import TableBody from '@material-ui/core/TableBody';
-
-import { db } from '../../services/firebase'
-import firebase from 'firebase';
-
-import Collapse from '@material-ui/core/Collapse';
-import IconButton from '@material-ui/core/IconButton';
-import KeyboardArrowDownIcon from '@material-ui/icons/KeyboardArrowDown';
-import KeyboardArrowUpIcon from '@material-ui/icons/KeyboardArrowUp';
-
-const useRowStyles = makeStyles({
-  root: {
-    '& > *': {
-      borderBottom: 'unset',
-    },
-  },
-});
-
-function Row(props) {
-  const { row } = props;
-  const [open, setOpen] = React.useState(false);
-  const classes = useRowStyles();
-
-  return (
-    <React.Fragment>
-      <TableRow className={classes.root}>
-        <TableCell>
-          <IconButton aria-label="expand row" size="small" onClick={() => setOpen(!open)}>
-            {open ? <KeyboardArrowUpIcon /> : <KeyboardArrowDownIcon />}
-          </IconButton>
-        </TableCell>
-        <TableCell component="th" scope="row">
-          {row.email}
-        </TableCell>
-        <TableCell align="right">{row.firstName}</TableCell>
-        <TableCell align="right">{row.lastName}</TableCell>
-        <TableCell align="right">{row.phone}</TableCell>
-        <TableCell align="right">{row.timezone}</TableCell>
-        <TableCell align="right">{row.university}</TableCell>
-      </TableRow>
-      <TableRow>
-        <TableCell style={{ paddingBottom: 0, paddingTop: 0 }} colSpan={6}>
-          <Collapse in={open} timeout="auto" unmountOnExit>
-            <div>Currently No Data to Show</div>
-          </Collapse>
-        </TableCell>
-      </TableRow>
-    </React.Fragment>
-  );
-}
-
 function TabPanel(props) {
   const { children, value, index, ...other } = props;
 
