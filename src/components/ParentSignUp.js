2a403715474d4cf1d94045e0184c5a03d9caae23
YasiTL <yasaman.most@gmail.com>
2021-07-14 07:31:00 -0500

    Remove unused link import on parent/tutor signup

--- src/components/ParentSignUp.js
+++ src/components/ParentSignUp.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ParentForm from './ParentForm';
-import { Link } from "react-router-dom"
 import { Grid, Box, Typography } from "@material-ui/core"
 
 function ParentSignUp() {
--- src/components/TutorSignUp.js
+++ src/components/TutorSignUp.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import TutorForm from './TutorForm';
-import { Link } from "react-router-dom"
 import { Grid, Box, Typography } from "@material-ui/core"
 
 
