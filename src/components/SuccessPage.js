36afb622d0c6145a7d6d51e0028890ea703165a3
BradEstacio <bradleyestacio@yahoo.com>
2021-07-15 02:18:17 -0500

    have success page link home

--- src/components/SuccessPage.js
+++ src/components/SuccessPage.js
@@ -1,10 +1,19 @@
-import React from 'react'
+import React from 'react';
+import { Link, Grid, Box, Typography } from "@material-ui/core";
 
 function SuccessPage() {
     return(
-        <div>
-            Test!
-        </div>
+        <Grid>
+            <Box display="flex" justifyContent="center">
+                <Typography variant="h2" align="center">Success!</Typography>
+            </Box>
+            <Box display="flex" justifyContent="center">
+                <Typography variant="div" align="center">
+                    We'll send you an email soon.&nbsp;
+                    <Link variant="text" color="secondary" component={Link} to="/success">Return to home page.</Link>
+                </Typography>
+            </Box>
+        </Grid>
     );
 }
 
