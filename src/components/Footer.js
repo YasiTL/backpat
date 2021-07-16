6ba43311eab97086e6fe285620da6d14680f1783
ah4na <ahanabha@buffalo.edu>
2021-07-16 14:37:34 -0400

    added footer component

---
+++ src/components/Footer.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import backpatlogo from "../assets/BackPatLogo.png"
+import teachingimage from '../assets/learningImage.png'
+import professorimage from '../assets/professor.png'
+import videocallimage from '../assets/videoCall.png'
+import {
+    BrowserRouter as Router, Switch, Route, Link,
+} from "react-router-dom"
+import { Box, Button, Container, Grid, Typography } from '@material-ui/core';
+import LinkedInIcon from '@material-ui/icons/LinkedIn';
+import FacebookIcon from '@material-ui/icons/Facebook';
+import InstagramIcon from '@material-ui/icons/Instagram';
+
+
+function Footer() {
+    return (
+        <div>
+            <footer style={{ maxWidth: "ls", paddingTop: "2%" }} mt={2}> {/* Footer*/}
+                <center>
+                    <Box bgcolor="#EF6C00">
+                        <Container>
+                            <Grid item xs={12} sm={4}>
+                                <Box><br /></Box>
+                                <Box color="black" component={Link} to="https://www.linkedin.com/company/backpat-tutoring/"><LinkedInIcon></LinkedInIcon></Box>
+                                <Box color="black" component={Link} to="https://www.facebook.com/backpattutoring/"><FacebookIcon></FacebookIcon></Box>
+                                <Box color="black" component={Link} to="https://www.instagram.com/backpattutoring/"><InstagramIcon></InstagramIcon></Box>
+                                <Box>
+                                    <Typography>Designed and devloped by Backpat.</Typography>
+                                    <Typography>All rights reserved. &copy;</Typography>
+                                </Box>
+                                <Box><br /></Box>
+                            </Grid>
+                        </Container>
+                    </Box>
+                </center>
+            </footer>
+        </div>
+    );
+}
+
+export default Footer;
--- src/components/Home.js
+++ src/components/Home.js
@@ -7,12 +7,12 @@ import {
   BrowserRouter as Router, Switch, Route, Link,
 } from "react-router-dom"
 import { Box, Button, Container, Grid, Typography } from '@material-ui/core';
-import LinkedInIcon from '@material-ui/icons/LinkedIn';
-import FacebookIcon from '@material-ui/icons/Facebook';
-import InstagramIcon from '@material-ui/icons/Instagram';
+
 
 import ParentSignUp from './ParentSignUp'
 import TutorSignUp from './TutorSignUp'
+
+import Footer from './Footer'
 
 //TODO: Add spacing between body and nav bar; add background to nav bar to make it more distinguishable; Possibly include social media links;
 function Home() {
@@ -42,6 +42,7 @@ function Home() {
           <Route path='/parentsignup' component={ParentSignUp} />
           <Route path='/tutorsignup' component={TutorSignUp} />
         </Switch>
+        <Footer />
       </div>
     </Router>
   );
@@ -116,26 +117,7 @@ function MainBody() {
       </Grid>
       <br />
       <br />
-      <footer style={{ maxWidth: "ls" }}> {/* Footer*/}
-        <center>
-          <Box bgcolor="#EF6C00">
-            <Container>
-              <Grid item xs={12} sm={4}>
-                <Box><br /></Box>
-                <Box color="black" component={Link} to="https://www.linkedin.com/company/backpat-tutoring/"><LinkedInIcon></LinkedInIcon></Box>
-                <Box color="black" component={Link} to="https://www.facebook.com/backpattutoring/"><FacebookIcon></FacebookIcon></Box>
-                <Box color="black" component={Link} to="https://www.instagram.com/backpattutoring/"><InstagramIcon></InstagramIcon></Box>
-                <Box>
-                  <Typography>Designed and devloped by Backpat.</Typography>
-                  <Typography>All rights reserved. &copy;</Typography>
-                </Box>
-                <Box><br /></Box>
-              </Grid>
-            </Container>
-          </Box>
-        </center>
-      </footer>
-    </div >
+    </div>
   );
 }
 
